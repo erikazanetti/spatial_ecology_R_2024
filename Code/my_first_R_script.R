f78b604fadffc2397e11249122003a4720014ad2
erikazanetti <erikazanetti.25@gmail.com>
2024-10-09 15:54:38 +0200

    Create my_first_R_script.R

---
+++ Code/my_first_R_script.R
@@ -0,0 +1,34 @@
+# Take home message:
+# object
+# assign
+# arrey
+# function(), example: c(), plot (), ...
+# comment 
+
+
+# R can be used as a calculator
+2+3
+
+sonia <- 2+3     # I assign the results of the opteration to an object
+sonia
+
+lea <- 3+4
+lea
+
+sonia + lea
+sonia ^ lea
+
+tommaso <- c(10, 20, 30, 50, 70) # this is an arrey
+erika <- c(1, 3, 5, 6, 7, 9)
+
+plot(tommaso, erika)
+# pch is changing the pointing characters
+plot(tommaso, erika, pch=19)
+
+# changing the color
+plot(tommaso, erika, pch=19, col="blue")
+
+# character exaggeration
+plot(tommaso, erika, pch=19, col="blue", cex=5)
+
+plot(tommaso, erika, pch=19, col="blue", cex=5, xlab="cannabis", ylab="dolphins")
