c0cca6e080086d06dbbe6e1cc063fb2d8188317a
erikazanetti <erikazanetti.25@gmail.com>
2024-11-10 15:28:47 +0100

    Update 01_my_first_R_script.R

--- Code/01_my_first_R_script.R
+++ Code/01_my_first_R_script.R
@@ -1,4 +1,4 @@
-# Take home message:
+# topics:
 # object
 # assign
 # arrey
@@ -9,6 +9,7 @@
 # R can be used as a calculator
 2+3
 
+# assign
 sonia <- 2+3     # I assign the results of the opteration to an object
 sonia
 
@@ -18,17 +19,17 @@ lea
 sonia + lea
 sonia ^ lea
 
+# arrey
 tommaso <- c(10, 20, 30, 50, 70) # this is an arrey
 erika <- c(1, 3, 5, 6, 7, 9)
 
+# function(): plot()
 plot(tommaso, erika)
-# pch is changing the pointing characters
-plot(tommaso, erika, pch=19)
+plot(tommaso, erika, pch=19) # pch is changing the pointing characters
 
-# changing the color
-plot(tommaso, erika, pch=19, col="blue")
+plot(tommaso, erika, pch=19, col="blue") # changing the color
 
-# character exaggeration
-plot(tommaso, erika, pch=19, col="blue", cex=5)
+
+plot(tommaso, erika, pch=19, col="blue", cex=5) # character exaggeration
 
 plot(tommaso, erika, pch=19, col="blue", cex=5, xlab="cannabis", ylab="dolphins")
