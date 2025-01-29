af533727a51bd8bebe4472204a6cd16456fd824f
erikazanetti <erikazanetti.25@gmail.com>
2025-01-29 12:08:21 +0100

    Update exam_code.R

--- Code/exam_code.R
+++ Code/exam_code.R
@@ -21,7 +21,7 @@ install_github("ducciorocchini/imageRy")
 library(imageRy)
 
 # Set the working directory
-setwd("C:/Users/Erika Zanetti/OneDrive/Desktop/spatial ecology in r/Browser_images 2017") # da cambiare perché devo mettere tutte immagini in una sola cartella
+setwd("C:/Users/Erika Zanetti/OneDrive - Alma Mater Studiorum Università di Bologna/spatial ecology in r/Browser_images 2017") # da cambiare perché devo mettere tutte immagini in una sola cartella
 
 # Import images while assigning them to an object
 m2017 <- rast("2017-01-26-00_00_2017-01-26-23_59_Sentinel-2_L2A_True_color.jpg") # True color (bands R, G and B)
@@ -37,34 +37,38 @@ plot(m2018f)
 
 # Visualize the images altogether in a multiframe
 par(mfrow=c(2,2))
-plot(m2017)
-title("True color 2017")
-plot(m2017f)
-title("False color 2017")
-plot(m2018)
-title("True color 2018")
-plot(m2018f)
-title("False color 2018")
+plot(m2017, main="True color 2017")
+plot(m2017f, main="False color 2017")
+plot(m2018, main="True color 2018")
+plot(m2018f, "False color 2018")
 
 dev.off()
 
 # Assigning red, green and blue bands of 2017's true color's image and 2017's false color's NIR band to their relative objects
-r2017 <- m2017[[1]]  # Red band
-g2017 <- m2017[[2]] # Green band
-b2017 <- m2017[[3]] # Blue band 
-nir2017 <- m2017f[[1]] # NIR band
-bands2017 <- c(r2017, g2017, b2017, nir2017)
+rm2017 <- m2017[[1]]  # Red band
+gm2017 <- m2017[[2]] # Green band
+bm2017 <- m2017[[3]] # Blue band 
+nirm2017 <- m2017f[[1]] # NIR band
+bandsm2017 <- c(rm2017, gm2017, bm2017, nirm2017) # Uniting all the bands
+bandsm2017c <- im.classify(bandsm2017,num_clusters=5) # Directly classifying the image with all the bands
+plot(bandsm2017c)
 
 # Doing the same thing for 2018
-r2018 <- m2018[[1]]
-g2018 <- m2018[[2]] 
-b2018 <- m2018[[3]] 
-nir2018 <- m2018f[[1]]
-bands2018 <- c(r2018, g2018, b2018, nir2018)
+rm2018 <- m2018[[1]]
+gm2018 <- m2018[[2]] 
+bm2018 <- m2018[[3]] 
+nirm2018 <- m2018f[[1]]
+bandsm2018 <- c(rm2018, gm2018, bm2018, nirm2018)
+bandsm2018c <- im.classify(bandsm2018,num_clusters=5)
+plot(bandsm2018c)
 
-#
-bands2017c <- im.classify(bands2017,num_clusters=5)
-bands2018c <- im.classify(bands2018,num_clusters=5)
+cl <- colorRampPalette(c("#21908CFF","#440154FF","#5DC863FF","#3B528BFF","#FDE725FF"))(100) 
+par(mfrow=c(1,2)) 
+plot(bandsm2017c, col=cl, main="Classified Image 2017") 
+plot(bandsm2018c, col=cl, main="Classified Image 2018")
+
+
+
 
 # Since im.classify() assigns class numbers randomly, I need to manually map the class numbers to meaningful names based on their frequency or your domain knowledge.
 class_names <- c("Cloudes", "Ocean", "Water bodies", "Dense veg.", "Less dense veg.")
@@ -77,8 +81,8 @@ class_colors <- c("Clouds" = "#21908CFF",    # Azzurro per le nuvole
 
 # da cambiare perché sono brutti
 par(mfrow=c(1,2))
-plot(bands2017c, col=class_colors, main="Classified Image 2017")
-plot(bands2018c, col=class_colors, main="Classified Image 2018")
+plot(bandsm2017c, col=class_colors, main="Classified Image 2017")
+plot(bandsm2018c, col=class_colors, main="Classified Image 2018")
 
 # Add a legend to make things clear  DA SISTEMARE IL POSIZIONAMENTO DELLA LEGENDA
 legend("topright", 
@@ -91,12 +95,17 @@ legend("topright",
 
 dev.off()
 
-# Frequencies
-f17 <- freq(bands2017c)
-tot17 <- ncell(bands2017c)
 
-f18 <- freq(bands2018c)
-tot18 <- ncell(bands2018c)
+cl <- colorRampPalette(c("#21908CFF","#440154FF","#5DC863FF","#3B528BFF","#FDE725FF"))(100)
+
+
+
+# Frequencies
+f17 <- freq(bandsm2017c)
+tot17 <- ncell(bandsm2017c)
+
+f18 <- freq(bandsm2018c)
+tot18 <- ncell(bandsm2018c)
 
 prop17 = f17/tot17
 perc17 = prop17*100
