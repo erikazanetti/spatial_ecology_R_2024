60533b92ef5cd269a47e0ec40b468647cf4586ee
erikazanetti <erikazanetti.25@gmail.com>
2025-02-05 11:55:04 +0100

    Update 00_EXAM_CODE.R

--- Code/00_EXAM_CODE.R
+++ Code/00_EXAM_CODE.R
@@ -7,7 +7,7 @@ install.packages(c("terra", "ggplot2", "patchwork", "viridis", "devtools"))
 library(terra) # For handling raster data (e.g., satellite images)
 library(ggplot2) # For creating graphs
 library(patchwork) # For composing multiple graphs in a single plot
-library(viridis) # For color palettes
+library(viridis) # For color palettes designed for colorblind people
 library(devtools) # For managing R packages
 
 # Install imageRy package from GitHub
