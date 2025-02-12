dd9361258d1e5691860647e3e00ef673054a7d6c
erikazanetti <erikazanetti.25@gmail.com>
2025-02-12 16:16:28 +0100

    Update 03_population_distribution.R

--- Code/03_population_distribution.R
+++ Code/03_population_distribution.R
@@ -1,72 +1,87 @@
-# Why populations disperse over time in a landscape in a certain way
-# topics:
-# find a file in the package
-# function vect()
-# Selecting presences
+# Why populations disperse over time in a landscape
 
-
-
-install.packages("sdm") # species distribution modelling
+# Install and load necessary packages
+install.packages("sdm")  # Species distribution modeling
 install.packages("terra")
 
 library(sdm)
 library(terra)
 
-# we want to make a subset of a data, to select only some points
-file <- system.file("external/species.shp", package="sdm")
+# Find and load a shapefile from the package
+file <- system.file("external/species.shp", package="sdm")  
 # [1] "C:/Users/Erika Zanetti/AppData/Local/R/win-library/4.4/sdm/external/species.shp"
-# in sdm folder there is a file called "external"  and in it there are some data we're gonna use today. Using system.file finds the name of the file
+# in sdm folder there is a file called "external", using system.file finds the name of the file
 # species.shp is related to data for species, shp means "shapefile" that is a type of file originally from a company
+
+# Convert the shapefile to a format R can use
+rana <- vect(file)  # Convert .shp file to a SpatVector object
+rana  # Display information about the dataset
+
+# Extract and plot occurrence data
+rana$Occurrence  
+plot(rana)  # Plot all data points (presence = 1, absence = 0)
+
+# Selecting presences (Occurrence = 1)
+pres <- rana[rana$Occurrence == 1]  
+pres$Occurrence  # Verify only presence points remain
+
 
 # let's use the data with a function to traslate .shp to type of file that R can use. spatvector is the one R can use, the function is called vect
 rana <- vect(file) # file is the object we named before instead of writing system.file etc...
-rana
-# the information we find: 1) it is a spatvector, 2) type of file (points), 3) extent, source, coordinates: wgs84, utm zone 30N, EPSG (code used for every single coordinate system), names: for each point there's a data recorded, occurance or not of a certain species, so if the species is present (1) or not (0) we write the numbers in each point 
+rana # Display information about the dataset
+# This includes details such as coordinate system (WGS84, UTM zone 30N)
+# and species occurrence data (1 = present, 0 = absent)
 
-rana$Occurrence
-plot(rana) # in this file some points represent the presence of the species (occurance = 1) and other the absence (occurance = 0)
+# Extract and plot occurrence data
+rana$Occurrence 
+plot(rana)  # Plot all data points (presence = 1, absence = 0)
 
 # Selecting presences
-pres <- rana[rana$Occurrence==1] # in sequel language (used here to select only data with occurence of 1) the equal is written with two ==, in sequel all finishes with ";"
+pres <- rana[rana$Occurrence == 1]  # Select only points where the species is present
+pres$Occurrence  # Verify only presence points remain
+
+# Plot presence and all data points side by side
+par(mfrow=c(1,2))  
+plot(rana)  # All data points
+plot(pres)  # Only presences
+
+
+# Selecting presences
+pres <- rana[rana$Occurrence==1] 
+# In SQL-like query languages, the equality operator is written as '=='
+# Unlike SQL, which ends statements with a ';', R does not require it.
 pres
 pres$Occurrence # and in fact now all occurrences are 1
 
-# exercise: plot rana and presences, one beside the other
-par(mfrow=c(1,2))
-plot(rana)
-plot(pres)
-
-# exercise: select absences
-abse <- rana[rana$Occurrence==0]
-abse$Occurrence
 dev.off()
-plot(abse)
 
-# exercise: plot in a multiframe presences besides absences
-par(mfrow=c(1,2))
-plot(pres)
-plot(abse)
+# Selecting absences
+abse <- rana[rana$Occurrence == 0] 
+abse$Occurrence 
+plot(abse)  # Plot only absence points
 
-# exercise: plot in a multiframe presences on top of absences
-par(mfrow=c(2,1))
-plot(pres)
-plot(abse)
+# Plot presences beside absences
+par(mfrow=c(1,2))  
+plot(pres)  
+plot(abse)  
 
-# exercise: plot the presence in blue together with absences in red
-plot(pres, col="blue", pch=19, cex=2)
-points(abse, col="red", pch=19, cex=2)
+# Plot presences on top of absences
+par(mfrow=c(2,1))  
+plot(pres)  
+plot(abse)  
 
-# Covariets
+# Plot presences in blue and absences in red
+plot(pres, col="blue", pch=19, cex=2)  # Blue points for presences
+points(abse, col="red", pch=19, cex=2)  # Red points for absences
+
+# Load elevation data
 elev <- system.file("external/elevation.asc", package="sdm")
-elev
+elevmap <- rast(elev) # Convert elevation file to raster format
+plot(elevmap)  # Plot elevation map
 
-elevmap <- rast(elev) # now we want to see the map of elevation
-elevmap
-plot(elevmap)
-
-# exercise: change the colors of the elevation map by the colorRampPalette function
+# Change elevation map colors using a color gradient
 cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
-plot(elevmap, col=cl)
+plot(elevmap, col=cl)  
 
-# exercise: plot the presence together with the elevation map
-points(pres, pch=19) # no need for par() because we put points on a plot made of a map (rast file), i punti sopra la mappa, quindi non separati ma uno sopra l'altro
+# Overlay presence points on the elevation map
+points(pres, pch=19)
