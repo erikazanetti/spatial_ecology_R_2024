b4af0587998752b293d4d004d058af0c938dae0f
erikazanetti <erikazanetti.25@gmail.com>
2025-02-12 15:49:08 +0100

    Update 02_population_density.R

--- Code/02_population_density.R
+++ Code/02_population_density.R
@@ -1,83 +1,64 @@
-# how to calculate the density of individuals in a population
-# topics:
-# using a dataset
-# extracting data
-# plotting a density map
-# plotting the maps one beside the other
-# changing colors to maps
-# increase the gradient
+# Install and load the required package for point pattern analysis
+# We use the spatstat package to deal with population ecology
+install.packages("spatstat")  
+library(spatstat)  
 
-# A package is needed for point pattern analysis, installing the spatstat package. We use spatstat package to deal with population ecology
-install.packages("spatstat")
+# Using the 'bei' dataset
+bei  # Displays the dataset
+plot(bei)  # Basic plot of the points in 'bei'
+plot(bei, pch = 19)  # Change point symbols
+plot(bei, pch = 19, cex = 0.5)  # Adjust point size
 
-# Recalling the package
-library(spatstat)
+# Additional datasets
+bei.extra  # Contains additional data such as elevation
+plot(bei.extra)  # Displays two images: elevation and gradient
 
-# let's use the bei dataset:
-bei
+# Extracting elevation data, first method
+elevation <- bei.extra$elev  # Extract elevation data
+plot(elevation)  # Plot only elevation
 
-plot(bei)
-plot(bei, pch=19) # pch -> simbolo diverso
-plot(bei, pch=19, cex=0.5) # cex -> puntini di dimensione 0.5
-
-# additional datasets
-bei.extra
-plot(bei.extra) # it shows me two images, one is elevation and one is graduation
-# bei is a vector that is composed by points ; bei.extra is composed by pixels that together create a raster
-
-
-# extracting data, first method:
-elevation <- bei.extra$elev
-plot(elevation) # now i only see elevation
-
-# second method to extract elements
+# Extracting elevation data, second method
 elevation <- bei.extra[[1]]
+
+# Create a density map from points
+densitymap <- density(bei)  
+plot(densitymap)
+points(bei, col = "green")
+
+# Plotting maps side by side
+par(mfrow = c(1,2))  
 plot(elevation)
-
-# density map starting from points, passing from points to a countinuous surface
-densitymap <- density(bei)
-
-plot(densitymap)
-points(bei, col="green")
-
-# plotting the maps one beside the other
-par(mfrow=c(1,2))
-plot(elevation2)
 plot(densitymap)
 
-# exercise: make a multiframe with maps one on top of the other
-par(mfrow=c(2,1))
-plot(elevation2)
-plot(densitymap)
+# Plotting maps stacked (one on top of the other)
+par(mfrow = c(2,1))  
+plot(elevation)  
+plot(densitymap)  
 
-# let's go back to the original plot(s)
-dev.off() # this is to clear off all graphs
-plot(elevation2)
+dev.off()  # Reset plotting settings
 
-# changing colors to maps
-cl <- colorRampPalette(c("red", "orange", "yellow"))(3) # dove 3 è il gradiente, aumentandolo diventa più sfumato
-plot(densitymap,col=cl)
+# Changing colors of the density map
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)  # 3-color gradient
+plot(densitymap, col = cl)  
 
-# increase the gradient
-cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
-plot(densitymap,col=cl)
+# Increasing gradient levels
+cl <- colorRampPalette(c("red", "orange", "yellow"))(10)  # More refined gradient
+plot(densitymap, col = cl)  
 
-# increase again
-cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
-plot(densitymap,col=cl)
+cl <- colorRampPalette(c("red", "orange", "yellow"))(100)  # High-gradient smooth transition
+plot(densitymap, col = cl)  
 
-# search in your browser for "colors in R"
+# Search online for "colors in R" for more customization options
 
 # Exercise: change the color ramp palette using different colors
-cl <- colorRampPalette (c("lightpink", "plum3", "mistyrose2"))(100)
+cl <- colorRampPalette(c("lightpink", "plum3", "mistyrose2"))(100)  
 
 # Exercise: build a multiframe and plot the densitymap with two different color ramp palettes one beside the other
-par(mfrow=c(1,2))
+par(mfrow = c(1,2))  
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)  
+plot(densitymap, col = cln)  
 
-cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
-plot(densitymap, col=cln)
-
-clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)
-plot(densitymap, col=clg)
+clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)  
+plot(densitymap, col = clg)  
 
 dev.off()
