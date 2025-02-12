7db5b4e56fdafe19582d437a4347cb64660bc421
erikazanetti <erikazanetti.25@gmail.com>
2025-02-12 15:37:54 +0100

    Update 01_my_first_R_script.R

--- Code/01_my_first_R_script.R
+++ Code/01_my_first_R_script.R
@@ -1,35 +1,23 @@
-# topics:
-# object
-# assign
-# arrey
-# function(), example: c(), plot (), ...
-# comment 
+# Basic R operations
 
+# Calculator operations
+2 + 3  
 
-# R can be used as a calculator
-2+3
+# Assigning values
+sonia <- 2 + 3  
+sonia  
+lea <- 3 + 4  
+lea  
+sonia + lea  
+sonia ^ lea  
 
-# assign
-sonia <- 2+3     # I assign the results of the opteration to an object
-sonia
+# Arrey
+tommaso <- c(10, 20, 30, 50, 70)  
+erika <- c(1, 3, 5, 6, 7, 9)  
 
-lea <- 3+4
-lea
-
-sonia + lea
-sonia ^ lea
-
-# arrey
-tommaso <- c(10, 20, 30, 50, 70) # this is an arrey. An arrey is a series of data. the function c is cramping all together the data
-erika <- c(1, 3, 5, 6, 7, 9)
-
-# function(): plot()
-plot(tommaso, erika)
-plot(tommaso, erika, pch=19) # pch is changing the pointing characters
-
-plot(tommaso, erika, pch=19, col="blue") # changing the color
-
-
-plot(tommaso, erika, pch=19, col="blue", cex=5) # character exaggeration
-
-plot(tommaso, erika, pch=19, col="blue", cex=5, xlab="cannabis", ylab="dolphins")
+# Plotting
+plot(tommaso, erika) 
+plot(tommaso, erika, pch = 19) # pch is changing the pointing characters
+plot(tommaso, erika, pch = 19, col = "blue")  # changing the color
+plot(tommaso, erika, pch = 19, col = "blue", cex = 5) # character exaggeration 
+plot(tommaso, erika, pch = 19, col = "blue", cex = 5, xlab = "Cannabis", ylab = "Dolphins")
